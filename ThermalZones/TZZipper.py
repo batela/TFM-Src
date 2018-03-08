8b259263e18f7b9ec97acf0d094819855dd52929
borja.deba <borja@deba.com>
2018-03-08 15:59:26 +0100

    Cambios para hacer plot y volcar los ficheros

--- ThermalZones/TZZipper.py
+++ ThermalZones/TZZipper.py
@@ -28,8 +28,12 @@ class TZZipper (object):
         self.id     = params
         self.data   = None
         self.logger = logging.getLogger("tzzipper")    
- #       self.roomNames =['R0T','R1','R2','R3','R4','R5','R6','R7T']
-        self.roomNames =['T0','T1','T3','T4','T5','T6','T7','T8','T9','T10','T11']    
+        #self.roomNames =['R0T','R1','R2','R3','R4','R5','R6','R7T']
+        self.roomNames =['T0','T1','T2','T3','T4','T5','T6','T7','T8','T9','T10','T11']    
+        self.k=7
+        self.period = 10
+        #self.days= 17
+        self.days= 0
     def clean (self):        
         self.data       = None
         
@@ -37,20 +41,26 @@ class TZZipper (object):
         
         self.logger.info ("Inicializamos los valores..")
         self.data       = data
-                
+        dayItems = (24*60//self.period)        
         rowData = []
+        
         for ro in self.roomNames:
             rowItem = self.data[[ro]]
-            rowData = np.append(rowData,rowItem[:96*17])
+            if (self.days > 0):
+                rowData = np.append(rowData,rowItem[:dayItems*self.days])
+            else:
+                rowData = np.append(rowData,rowItem)
         
         self.logger.info ("fin de inicializacion..")
-        return np.resize (rowData,(len(rowData)//96,96))
+        
+        return np.resize (rowData,(len(rowData)//dayItems,dayItems))
     
     def clusterizeKmeans (self,groups,X):
         self.logger.info ("Inicio clusterizaccion..")
     
-        kmeans = KMeans(n_clusters=groups, random_state=0).fit(X)
-        ll = kmeans.labels_
+        # Para revisar
+        #kmeans = KMeans(n_clusters=groups, random_state=0).fit(X)
+        #ll = kmeans.labels_
    
         self.logger.info ("Fin cluseterizacion")
         
@@ -59,8 +69,8 @@ class TZZipper (object):
         self.logger.info ("Inicio clusterizaccion por hierarchical..")
         Z = linkage(X, 'ward')
         c, coph_dists = cophenet(Z, pdist(X))
-        k=3
-        cl = fcluster(Z, k, criterion='maxclust')
+        
+        cl = fcluster(Z, self.k, criterion='maxclust')
         
         self.plotter(Z)
         return cl
--- ThermalZones/TZmain.py
+++ ThermalZones/TZmain.py
@@ -27,8 +27,10 @@ def loadFileData (filepath):
         #filename = 'PumpGround_allData.csv'
         #data = pd.read_csv(filepath+filename, sep=';', decimal = ',',thousands = '.', usecols=['DATE', 'Timpout36', 'Tretout37','Timpin22','Tretin23','Econsump', 'ONOFF'], parse_dates=['DATE'])
         
+        
+        filename = 'FHP-20141008.csv'        
         filename = 'datosvivienda_test.csv'
-        filename = 'FHP-20141008.csv'
+        filename = 'ed700.csv'
         #usecols = ['Fecha','geotech_raw_36_Temperatura_T1','geotech_raw_37_Temperatura_T2','geotech_raw_256_EnergiaActiva','geotech_raw_2_T_Aire_Exterior','geotech_raw_22_EP_Impulsion_T','geotech_raw_23_EP_Retorno_T','geotech_raw_24_Caudal']
         data = pd.read_csv(filepath+filename, sep=';', decimal = '.')
         return data
@@ -36,21 +38,52 @@ def loadFileData (filepath):
 
 def saveFileData (filepath,data):
                 
-        #self.logger.info ("Loading data from file....")                 
+        #self.logger.info ("Loading data from file....")            di =     
         filename = 'pydata.csv'        
         numpy.savetxt(filepath+filename, data, fmt='%.4f', delimiter=';')
         
         return data
 
+def calculaStadist(data,cl):
+        dias = 579
+        rooms = 12
+        idx = -1
+        di = None
+        for ro in list(range (0,rooms)):
+            idx +=1
+            l = list(cl[dias*idx:dias*(idx+1)])
+            for it in list(range (1,numpy.amax(cl)+1)):
+                di = dict((it,l.count(it)) for it in set(l))
+            logger.info ("Found data for room " + str (ro) +" : "+ str(di))
+                
 def auxPlotter(data,cl):
                 
     idx = -1
+    count = -1;
+    ptidx = -1
+    
     colour=['blue','green','red','orange','cyan','black','pink','magenta']
+    # Two subplots, the axes array is 1-d
+    #f, ptarr = plt2.subplots(2, sharex=True)
     for it in data:
-        idx+=1
-        plt2.plot(it,color=colour[cl[idx]])
-    plt2.savefig('../Images/myfig')
+        idx+=1 
+        #count+=1
+        #if ((count %579) == 0):
+        #    ptidx+=1
+        plt2.plot(it,color=colour[cl[idx-1]])
+        plt2.savefig('../Images/myfigdv')
+        ##if ((idx %579) == 578):
+        ##    count+=1    
+            #plt2.savefig('../Images/mybfigdv_'+str(count))
 
+#f, ptarr = plt2.subplots(12, sharex=True)
+#    for it in data:
+#        idx+=1 
+#        count+=1
+#        if ((count %579) == 0):
+#            ptidx+=1
+#        ptarr[ptidx].plot(it,color=colour[cl[idx-1]])        
+#    plt2.savefig('../Images/myfig')
 
 if __name__ == "__main__":
     
@@ -63,7 +96,7 @@ if __name__ == "__main__":
     FORMAT = '%(levelname)s - %(asctime)s - %(filename)s::%(funcName)s - %(message)s'
     #logging.basicConfig(level=logging.DEBUG, format = '%(levelname)s - %(asctime)s - %(filename)s:%(lineno)s - %(message)s')
     logging.basicConfig(level=logging.DEBUG, format = FORMAT)
-    logger = logging.getLogger("hpplanner")
+    logger = logging.getLogger("tzzipper")
     
     handler = logging.FileHandler('hello.log')
     handler.setLevel(logging.DEBUG)
@@ -82,8 +115,8 @@ if __name__ == "__main__":
     saveFileData (filepath,data)
     #hpp.clusterize (4,data)
     cl = hpp.clusterizeHClust (data)
-    
-    auxPlotter(data,cl)    
-    
+#    auxPlotter(data,cl)    de momentono hago las graficas
+    calculaStadist(data,cl)
+        
     logger.debug("Process ended...")
     
