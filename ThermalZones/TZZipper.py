2793356047f732c0fb6d9aaf0d7b14521f40692f
btl <btl@blt.com>
2018-03-11 20:14:43 +0100

    Modificaciones para hacer plots en fichero y crear variables mas generales

--- ThermalZones/TZZipper.py
+++ ThermalZones/TZZipper.py
@@ -8,28 +8,25 @@ Created on Mon Mar  5 09:26:57 2018
 
 import logging
 import numpy as np
-import pandas as pd
-from sklearn.cluster import KMeans
-
 from matplotlib import pyplot as plt
 from scipy.cluster.hierarchy import dendrogram, linkage
-from scipy.cluster.hierarchy import cophenet
-from scipy.spatial.distance import pdist
 from scipy.cluster.hierarchy import fcluster
 
-from scipy.cluster.hierarchy import fcluster
+#from sklearn.cluster import KMeans
+#from scipy.cluster.hierarchy import cophenet
 
 class TZZipper (object):
 
-    def __init__(self, params):
+    def __init__(self, params,roomN):
         '''
         Constructor esto es un comenario
         '''
         self.id     = params
         self.data   = None
         self.logger = logging.getLogger("tzzipper")    
+        self.roomNames = roomN
         #self.roomNames =['R0T','R1','R2','R3','R4','R5','R6','R7T']
-        self.roomNames =['T0','T1','T2','T3','T4','T5','T6','T7','T8','T9','T10','T11']    
+        #self.roomNames =['T0','T1','T2','T3','T4','T5','T6','T7','T8','T9','T10','T11']    
         self.k=None
         self.period = None
         self.days= None
@@ -46,11 +43,10 @@ class TZZipper (object):
         
         self.logger.info ("Inicializamos los valores..")
         
-        self.k=7
+        self.k=k
         self.period = period
         #self.days= 17
         self.days= days
-        
         self.data       = data
         dayItems = (24*60//self.period)        
         rowData = []
@@ -83,28 +79,35 @@ class TZZipper (object):
         
         acceleration = np.diff(last, 2)  # 2nd derivative of the distances
         acceleration_rev = acceleration[::-1]
-        plt.plot(idxs[:-2] + 1, acceleration_rev)
-        plt.show()
-        k = acceleration_rev.argmax() + 2  # if idx 0 is the max of this we want 2 clusters
-        self.logger.info ("Cluster calculation" + str(k))
         
+        plt.plot(idxs[:-2] + 1, acceleration_rev)
+        plt.savefig('../Images/elbow')
+        plt.close()
+                
+        k = acceleration_rev.argmax() + 2  # if idx 0 is the max of this we want 2 clusters
+        self.logger.info ("Cluster calculation: " + str(k))
+         
+        return k
         
     def clusterizeHClust (self,X):
         self.logger.info ("Inicio clusterizaccion por hierarchical..")
         Z = linkage(X, 'ward')
         
-        self.elbowMethod (Z)
-        
-        #c, coph_dists = cophenet(Z, pdist(X))
+        kelbow = self.elbowMethod (Z)
+       #c, coph_dists = cophenet(Z, pdist(X))
+## BTL la mejor resolucion sale 3 pero utilizar 4 clusters
+## me parece que da mejor resultado
         cl = fcluster(Z, self.k, criterion='maxclust')
         
         self.plotter(Z)
+        
+        self.logger.info ("F clusterizaccion por hierarchical..")
         return cl
     
     
     def plotter (self,Z):
         
-        self.logger.info ("Inicio ploting dendongram..")
+        self.logger.info ("Inicio ploting dendongram.." + str(self.k))
         
         #plt.figure(figsize=(25, 10))
         plt.title('Hierarchical Clustering Dendrogram')
@@ -113,10 +116,12 @@ class TZZipper (object):
         dendrogram(
             Z,
             truncate_mode='lastp',  # show only the last p merged clusters
-            p=5,  # show only the last p merged clusters
+            p=int(self.k),  # show only the last p merged clusters
             leaf_rotation=90.,
             leaf_font_size=12.,
             show_contracted=True,  # to get a distribution impression in truncated branches
         )
-        plt.show()
+        plt.savefig('../Images/dendrogram')
+        plt.close()
+        
         self.logger.info ("Fin plotting..")
--- ThermalZones/TZmain.py
+++ ThermalZones/TZmain.py
@@ -18,21 +18,33 @@ import numpy
 import imp
 import TZZipper
 from matplotlib import pyplot as plt2
+from scipy.spatial.distance import pdist, squareform
+
 
 ## Esto no debería estar aqui.... pero por simplificar..
-period = 10
-days=0
-k = 4
+## BTL Variables de entrada
+period = 15 ## Periodo entre datos en minutos
+days=0      ## Si 0 se toman todos los datos del fichero, sino lo "dias" primeros
+k = 4       ## Numero de clusteres 
+rooms = 8   ## Numero de zonas/estancias
+roomNames =['R0T','R1','R2','R3','R4','R5','R6','R7T']
+#roomNames =['T0','T1','T2','T3','T4','T5','T6','T7','T8','T9','T10','T11']    
+        
+## BTL Variables de salida
+distances   = None ## Matrix de distancias por dia
+clusteres   = None ## Clusters encontrados 
+distribut   = None ## Distribcion de cada zona en clusteres
+
 
 def loadFileData (filepath):
                 
         logger.info ("Loading data from file....")         
 
-        filename = 'FHP-20141008.csv'                
+#        filename = 'ed700.csv'
+#        filename = 'FHP-20141008.csv'                
         filename = 'datosvivienda_test.csv'
-        filename = 'ed700.csv'        
                 
-        fulldata = pd.read_csv(filepath+filename, sep=';', decimal = '.')
+        fulldata = pd.read_csv(filepath+filename, sep=',', decimal = '.')
         
         logger.debug ("Looking for proper daytypes..")         
         data = fulldata[(pd.to_datetime(fulldata['Time']).dt.weekday < 5)]
@@ -49,53 +61,67 @@ def saveFileData (filepath,data):
         
         return data
 
+def groupByDays(data):
+        
+        logger.info ("Starting groupByDays")
+        dias = len(data)//(rooms)
+        dayItems = (24*60//period) 
+        dayzones = numpy.zeros((dias, rooms, dayItems))
+        
+        logger.debug ("Creating aggrupation")       
+        for idD in range (0,dias):
+            for idR in range (0,rooms):
+#                logger.debug ("Values:" + str( data[(dias*idR)+idD]))
+                dayzones[idD][idR] =  data[(dias*idR)+idD]
+        
+        logger.debug ("Calculating distance matrix")       
+        distances = numpy.zeros((dias, rooms, rooms))
+        for idD in range (0,dias):
+            distances[idD] = squareform(pdist(dayzones[idD], 'seuclidean', V=None) )       
+        
+        logger.info ("Finish groupByDays")
+    
 def calculaStadist(data,cl):
         
-        rooms = 12
         dias = len(cl)//(rooms)
-        
         idx = -1
-        di = None
         for ro in list(range (0,rooms)):
             idx +=1
             l = list(cl[dias*idx:dias*(idx+1)])
             for it in list(range (1,numpy.amax(cl)+1)):
-                di = dict((it,(l.count(it)*100//dias)) for it in set(l))
-            logger.info ("Found data for room " + str (ro) +" : "+ str(di))
+                distribut = dict((it,(l.count(it)*100//dias)) for it in set(l))
+            logger.info ("Found data for room " + str (ro) +" : "+ str(distribut))
                 
 def auxPlotter(data,cl):
                 
-    idx = -1
-    count = -1;
-    ptidx = -1
-    
-    colour=['blue','green','red','orange','cyan','black','pink','magenta']
-    # Two subplots, the axes array is 1-d
-    #f, ptarr = plt2.subplots(2, sharex=True)
-    for it in data:
-        idx+=1 
-        #count+=1
-        #if ((count %579) == 0):
-        #    ptidx+=1
-        plt2.plot(it,color=colour[cl[idx-1]])
-        plt2.savefig('../Images/myfigdv')
-        ##if ((idx %579) == 578):
-        ##    count+=1    
-            #plt2.savefig('../Images/mybfigdv_'+str(count))
+        idx = -1
+        idxRo = 0
+        count = -1;
+        ptidx = -1
+        dias = len(cl)//(rooms)
+        colour=['blue','green','red','orange','cyan','black','pink','magenta']
+        
+        #f, ptarr = plt2.subplots(rooms, sharex=True)
+        
+        plt2.close()
+        for it in data:
+            idx+=1 
+            count+=1
+            if ((count %dias) == 0):
+                ptidx+=1
+            #ptarr[ptidx].plot(it,color=colour[cl[idx-1]])
+            plt2.plot(it,color=colour[cl[idx-1]])        
+            ## Prints a chart per room
+            if ((idx %dias) == (dias-1)):   
+                plt2.savefig('../Images/zone_'+str(idxRo))
+                plt2.close()
+                idxRo+=1
 
-#f, ptarr = plt2.subplots(12, sharex=True)
-#    for it in data:
-#        idx+=1 
-#        count+=1
-#        if ((count %579) == 0):
-#            ptidx+=1
-#        ptarr[ptidx].plot(it,color=colour[cl[idx-1]])        
-#    plt2.savefig('../Images/myfig')
 
 if __name__ == "__main__":
     
     filepath='../Repo/'
-#    imp.reload(IHPPlanner)
+    imp.reload(TZZipper)
     imp.reload(logging)
     
     
@@ -115,15 +141,26 @@ if __name__ == "__main__":
     
     logger.info ("Starting process..")
     
+## BTL creamos la clase que utilizaremos    
+    hpp = TZZipper.TZZipper("mytest",roomNames)
     
-    hpp = TZZipper.TZZipper("mytest")
+## BTL Inicializacion y creacion de estructura de datos que utilizaremos.
+## "data" es una matriz en el que se ordenan por cada zona los datos corres-
+## pondientes a sus "dias" de forma consecutiva. Es decir las primeras n filas 
+## pertenecen a los n "dias" de la primera zona
     data = hpp.initialize(loadFileData (filepath),k,period,days) # Para RV 3,15,17 Para 700 7,10,0
-            
+
+## BTL funcion auxiliar            
     saveFileData (filepath,data)
-    #hpp.clusterize (4,data)
-    cl = hpp.clusterizeHClust (data)
-#    auxPlotter(data,cl)    de momentono hago las graficas
-    calculaStadist(data,cl)
+## BTL funcion que calcula la distancia por dia para cada una de las zonas
+    groupByDays (data)
+    
+#   hpp.clusterize (4,data)
+
+## BTL realiza la clusterizacion , plotea y calculo de estadisticas...
+    clusteres = hpp.clusterizeHClust (data)
+    auxPlotter(data,clusteres)    
+    calculaStadist(data,clusteres)
         
     logger.debug("Process ended...")
     
